94b5dc41bb7db01da9d762a2e77be2649bae83bc
Peter Kreppein <pmkreppein@gmail.com>
2018-10-10 01:13:13 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,28 @@
+class CashRegister
 
+  attr_accessor :items, :disc, :tot, :last_transaction
+
+  def initialize(disc=0)
+    @tot = 0
+    @disc = disc
+    @items = []
+  end
+  def add_item(title, amount, qty=1)
+    self.tot += amount * qty
+    qty.times do
+      items << title
+    end
+    self.last_transaction = amount * qty
+  end
+  def void_last_transaction
+    self.tot = self.tot - self.last_transaction
+  end
+end
+def apply_discount
+  if disc != 0
+    self.tot = (tot * ((100.0 - disc.to_f)/100)).to_i
+    "After the disc, the tot comes to $#{self.tot}."
+  else
+    "There is no disc to apply."
+  end
+end
